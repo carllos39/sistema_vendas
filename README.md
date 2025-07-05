529a51c9984cc3518057244b5533335441f49ad6
carllos39 <140573083+carllos39@users.noreply.github.com>
2025-07-04 22:12:35 -0300

    atualizar

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# sistema_vendas
 # Sistema Vendas
 
 Sistema desenvolvido em PHP e MySQL, Orientado a Objetos e com gerenciamento de sessão de Usuário.
--- model/ModelTeste.php
+++ model/ModelTeste.php
@@ -1,29 +1,31 @@
 <?php
-require_once  __DIR__ . '/Categoria.php';
-require_once __DIR__ . '/Entidade.php';
-require_once   __DIR__ . '/FormaPagamento.php';
-require_once  __DIR__ . '/Usuario.php';
+
+require_once './Entidade.php';
+require_once './Categoria.php';
+require_once './FormaPagamento.php';
+require_once './Usuario.php';
+
+header('Content-Type: text/plain;');
+
 echo "======================================\n";
 echo "=== INICIANDO OS TESTES DE MODELS ====\n";
 echo "======================================\n\n";
 
-
-// CATEGORIA E FORMA DE PAGAMENTO
-echo "1. Criando Categoria e Forma de Pagamento \n";
+// 1 - CATEGORIA E FORMA DE PAGAMENTO
+echo "1. Criando Categoria e Forma de Pagamento \n\n";
 $categoriaEletro = new Categoria(1, 'Eletronicos', 'Dispositvios que encaixam na tomada', true);
 $formaPagametno = new FormaPagamento(1, 'Cartão de Crédito', 'Pagamento via cartão', true);
-
 
 print_r($categoriaEletro);
 print_r($formaPagametno);
 echo "\n";
+
 // 2 - CRIANDO USUÁRIO (CLIENTE)
-echo "1. Criando Usuário (Cliente) \n\n";
+echo "2. Criando Usuário (Cliente) \n\n";
 $cliente = new Usuario(10, 'João da Silva', 'joao.silva', 'super_senha', 'joao@mail.com',
     '11999998888', '123.456.789-00', false, true);
-    print_r($cliente);
-    echo "\n";
-    // 3 - PRODUTOS QUE PERTENCEM A UMA CATEGORIA
 
+print_r($cliente);
+echo "\n";
 
-?>
+// 3 - PRODUTOS QUE PERTENCEM A UMA CATEGORIA
